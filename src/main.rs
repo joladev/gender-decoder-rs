6d5486ef45f410619d897ebae8552c4946214ba5
Johanna Larsson <johanna.a.m.larsson@gmail.com>
2017-01-06 23:42:47 +0100

    Implemented persistence using the file system

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,17 @@ extern crate rocket_contrib;
 extern crate rocket;
 extern crate serde_json;
 #[macro_use] extern crate serde_derive;
+extern crate rand;
 
 use std::io;
 use std::fs::File;
 use rocket::response::NamedFile;
 use rocket::request::Form;
 use rocket_contrib::Template;
+use rocket::response::Redirect;
+use std::io::Write;
+use std::io::Read;
+use rand::Rng;
 
 #[derive(Serialize)]
 struct TemplateContext {
@@ -43,18 +48,32 @@ fn get_words(path: &str) -> Vec<String> {
     serde_json::from_reader(file).expect(format!("Failed to deserialize json: {}", path).as_str())
 }
 
+const BASE62: &'static [u8] = b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+fn get_id() -> String {
+    let size = 10;
+    let mut id = String::with_capacity(size);
+    let mut rng = rand::thread_rng();
+        for _ in 0..size {
+            id.push(BASE62[rng.gen::<usize>() % 62] as char);
+        }
+    id
+}
+
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
     NamedFile::open("templates/index.html")
 }
 
-#[post("/decode", format = "application/x-www-form-urlencoded", data = "<ad_form>")]
-fn decode(ad_form: Form<Ad>) -> Template {
-    let ad_text = &ad_form.get().ad_text;
+#[get("/<id>")]
+fn get_by_id(id: String) -> Template {
+    let mut ad_text = &mut String::new();
+
+    File::open(format!("uploads/{id}", id = id)).unwrap().read_to_string(ad_text);
     let feminine_words = get_words("src/feminine_words.json");
     let masculine_words = get_words("src/masculine_words.json");
-    let feminine_results = ad_decoder(ad_text, feminine_words);
-    let masculine_results = ad_decoder(ad_text, masculine_words);
+    let feminine_results = ad_decoder(&ad_text, feminine_words);
+    let masculine_results = ad_decoder(&ad_text, masculine_words);
 
     let context = TemplateContext {
         feminine_words: feminine_results,
@@ -64,8 +83,19 @@ fn decode(ad_form: Form<Ad>) -> Template {
     Template::render("decoded", &context)
 }
 
+#[post("/decode", data = "<ad_form>")]
+fn decode(ad_form: Form<Ad>) -> Redirect {
+    let id = get_id();
+    let path = format!("uploads/{id}", id = id);
+    let ad_text = &ad_form.get().ad_text;
+
+    File::create(path).unwrap().write_all(ad_text.as_bytes());
+
+    Redirect::to(&format!("/{id}", id = id))
+}
+
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index, decode])
+        .mount("/", routes![index, decode, get_by_id])
         .launch();
 }
